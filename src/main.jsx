305cf1a3f4c5744e84a2bd5c37d27c9cd3b0f30c
jeevan sai bandi <bandijeevansai@gmail.com>
2026-02-24 22:19:00 +0530

    error resolve

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <StrictMode>
     <App />
-  </HashRouter>,
+  </StrictMode>,
 )
